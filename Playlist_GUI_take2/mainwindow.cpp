b23a6d85d22d05fd0ec446db2503883ea55cadfa
codies27 <codies27@bu.edu>
2016-04-19 14:12:28 -0400

    Update mainwindow.cpp
    faster & fixed empty text box issue

--- Playlist_GUI_take2/mainwindow.cpp
+++ Playlist_GUI_take2/mainwindow.cpp
@@ -10,12 +10,12 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    //SongInfo songInfo;
-    //songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day01.txt");
-    //songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day02.txt");
-    //songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day03.txt");
-    //songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day04.txt");
-
+    SongInfo songInfo;
+    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day01.txt");
+    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day02.txt");
+    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day03.txt");
+    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day04.txt");
+    
     QStringList DisBeTrending = (QStringList()<<"#dis"<<"#be"<<"#trending");
     ui->trending->insertItems(0, DisBeTrending);
 }
@@ -49,16 +49,16 @@ void MainWindow::on_listWidget_itemDoubleClicked(QListWidgetItem *item)
 
 void MainWindow::on_lineEdit_textChanged(const QString &arg1)
 {
-    SongInfo songInfo;
-    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day01.txt");
-    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day02.txt");
-    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day03.txt");
-    songInfo.loadPlaylistFile("/Users/codiesmith/Downloads/day04.txt");
 
     QString current = ui->lineEdit->displayText();
-
     QStringList topFourSongs = songInfo.findFourSongs(current);
     ui->listWidget->clear();
+    if(current == ""){
+        QStringList blank = (QStringList()<<"");
+        ui->listWidget->insertItems(0,blank);
+    }
+    else{
     ui->listWidget->insertItems(0, topFourSongs);
+    }
 
 }
